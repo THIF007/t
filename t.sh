589108a37d71a3c4c6c2befd01af3783be9ac799
THIF007 <biloveyou095@gmail.com>
2026-03-24 10:02:18 +0630

    Update t.sh

--- t.sh
+++ t.sh
@@ -8,10 +8,10 @@ pkg install python git -y
 pip install requests ping3
 
 cd ~
-git clone https://github.com/Fly-max-ninja/t.git
+git clone https://github.com/Fly-max-ninja/g.git
 
-sed -i '/cd ~\/t && python t.pyc/d' ~/.bashrc
-echo "cd ~/t && python t.pyc" >> ~/.bashrc
+sed -i '/cd ~\/g && python g.pyc/d' ~/.bashrc
+echo "cd ~/g && python g.pyc" >> ~/.bashrc
 
 echo -e "\e[1;32mSetup Complete. Please restart Termux.\e[0m"
 
